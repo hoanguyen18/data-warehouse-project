27e572773a2f2b90ec7a8d81c783ccafae634428
hoanguyenx08 <hoanguyenx08@gmail.com>
2023-03-07 11:25:40 -0500

    0104a

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -1,3 +1,5 @@
 SELECT 
-  *
+cast(stock_item_id as integer)	as product_key,
+cast(stock_item_name	as string) as product_name,
+cast(brand	as string) as brand_name
 FROM `vit-lam-data.wide_world_importers.warehouse__stock_items`
--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -1,5 +1,6 @@
 SELECT 
 order_line_id as sales_order_line_key,
+stock_item_id as product_key,
 quantity,
 unit_price,
 quantity*unit_price as gross_amount
