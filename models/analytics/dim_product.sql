144605ba957a01b9b1ddb51bdeb06e6522b15793
hoanguyenx08 <hoanguyenx08@gmail.com>
2023-05-16 11:20:29 -0400

    0107b: flatten data into model dim_product

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -5,19 +5,25 @@ with dim_product__source as (
 dim_product__rename_column as (
   select stock_item_id as product_key,
   stock_item_name	as product_name,
-  brand as brand_name 
+  brand as brand_name,
+  supplier_id as supplier_key 
   from dim_product__source
 ),
 
 dim_product__cast_type as (
   select 
-  cast(stock_item_id as integer) as product_key,
-  cast(stock_item_name as string) as product_name,
-  cast(brand as string) as brand_name
-  from dim_product__source
+  cast(product_key as integer) as product_key,
+  cast(product_name as string) as product_name,
+  cast(brand_name as string) as brand_name,
+  cast(supplier_key as integer) as supplier_key
+  from dim_product__rename_column
 )
 -- nen ghi ro cac cot, thay vi dung * de nguoi doc hieu ro
-select product_key,
-product_name,
-brand_name
-from dim_product__cast_type
+select dim_product.product_key,
+dim_product.product_name,
+dim_product.brand_name, 
+dim_product.supplier_key,
+dim_supplier.supplier_name
+from dim_product__cast_type as dim_product 
+left join {{ref('dim_supplier')}} as dim_supplier
+on dim_product.supplier_key = dim_supplier.supplier_key
--- models/analytics/dim_supplier.sql
+++ models/analytics/dim_supplier.sql
@@ -1,3 +1,21 @@
-SELECT 
+with dim_supplier__source as(
+  SELECT 
   *
 FROM `vit-lam-data.wide_world_importers.purchasing__suppliers`
+),
+
+dim_supplier__rename_column as (
+  select supplier_id as supplier_key,
+  supplier_name
+  from dim_supplier__source
+),
+
+dim_supplier__cast_type as(
+  select cast(supplier_key as integer) as supplier_key,
+  cast(supplier_name as string) as supplier_name
+  from dim_supplier__rename_column
+)
+
+select supplier_key, supplier_name
+from dim_supplier__cast_type
+
