fc9a9c79eb14b769e2a9f21c2c507f1708da5f63
hoanguyenx08 <hoanguyenx08@gmail.com>
2023-05-20 11:45:35 -0400

    0108 a  add is_chiller_stock boolean onto dim_product

--- models/analytics/dim_customer.sql
+++ models/analytics/dim_customer.sql
@@ -6,6 +6,7 @@ FROM `vit-lam-data.wide_world_importers.sales__customers`),
 dim_customer__rename_column as(
 select 
 customer_id as customer_key,
+is_on_credit_hold as is_on_credit_hold_boolean,
 customer_name,
 customer_category_id as customer_category_key,
 buying_group_id as buying_group_key
@@ -17,7 +18,19 @@ dim_customer__cast_type as(
   cast(customer_key as integer) as customer_key,
   cast(customer_name as string) as customer_name,
   cast(customer_category_key as integer) as customer_category_key,
-  cast(buying_group_key as integer) as buying_group_key
+  cast(buying_group_key as integer) as buying_group_key,
+  cast(is_on_credit_hold_boolean as boolean) as is_on_credit_hold_boolean
+)
+  from dim_customer__rename_column,
+
+dim_customer__convert as(
+  select
+  cast(customer_key as integer) as customer_key,
+  cast(customer_name as string) as customer_name,
+  cast(customer_category_key as integer) as customer_category_key,
+  cast(buying_group_key as integer) as buying_group_key,
+  cast(is_on_credit_hold_boolean as boolean) as is_on_credit_hold_boolean
+)
   from dim_customer__rename_column
 )
 
